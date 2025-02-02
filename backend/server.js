d3bf9dddffa6216b9d7491478a554682da089987
Irfan <irfanjankhan7860@gmail.com>
2025-02-02 11:56:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",  // Replace with your frontend URL
+    origin: "https://skillswap-1-tn5u.onrender.com",  // Replace with your frontend URL
     methods: ["GET", "POST"]
   }
 });
